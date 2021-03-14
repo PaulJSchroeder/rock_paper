a3c9e067a82bb574eaf8745d001dd45c7888158e
Paul Schroeder <ps8thrice@yahoo.com>
2021-03-13 22:56:10 -0600

    fixed the three functions so they work together properly

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,16 @@ let playerScore = 0;
 let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
 //input playerSelection and computerSelection
+    playerSelection = prompt("Enter: 'Rock' 'Paper' or 'Scissors'");
+    computerSelection = computerPlay(); 
+//Declare the winner of the round    
+    let playerWins = "You chose: " + playerSelection + ", Computer chose: " + computerSelection + 
+    ", You win this round! "
+    let playerLoses = "You chose: " + playerSelection + ", Computer chose: " + computerSelection + 
+    ", Computer wins this round! " 
+    let playerDraws = "You chose: " + playerSelection + ", Computer chose: " + computerSelection + 
+    ", Its a draw! "
+    let errorMessage = "Please enter a valid response.";
 //compare the two values and decide which one beats the other
     if (playerSelection.toUpperCase() === "ROCK") {
         if (computerSelection === "Scissors") {
@@ -54,34 +64,20 @@ function playRound(playerSelection, computerSelection) {
     else {
         return errorMessage;
     }
-}
-
-const playerSelection = prompt("Enter: 'Rock' 'Paper' or 'Scissors'");
-let computerSelection = computerPlay();
-
-//declare the winner
-    let playerWins = "You chose: " + playerSelection + " Computer chose: " + computerSelection + 
-    " You win this round! "
-    let playerLoses = "You chose: " + playerSelection + " Computer chose: " + computerSelection + 
-    " Computer wins this round! " 
-    let playerDraws = "You chose: " + playerSelection + " Computer chose: " + computerSelection + 
-    " Its a draw! "
-    let errorMessage = "Please enter a valid response.";
-    
+   
+}   
 //use playRound to play 5 rounds
  function game(playerSelection, computerSelection) {
     for (let round = 0; round < 5; round++) {
-        computerPlay();
-        playerSelection = prompt("Enter: 'Rock' 'Paper' or 'Scissors'");
         console.log(playRound(playerSelection, computerSelection));
         console.log("Your score: " + playerScore + " Computer score: " + computerScore);
     }
  }
- 
+ console.log(game());
 
  
  //report the winner at the end   
 
-console.log(game(playerSelection, computerSelection));
+
 
 
